8dbb9717d6ebd8817279d5ae1d1bcd9c28905bd5
root <root>
2023-03-21 11:59:07 +0800

    文件上传代码

--- src/main/java/top/bulk/big/BulkBigFileDemoApplication.java
+++ src/main/java/top/bulk/big/BulkBigFileDemoApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BulkBigFileDemoApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(BulkBigFileDemoApplication.class, args);
+        SpringApplication.run(BulkBigFileDemoApplication.class,args);
         System.out.println("访问地址: http://localhost:8080/page/index.html");
     }
-
 }
